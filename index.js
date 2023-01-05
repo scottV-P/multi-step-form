4c9ec1afb4deba8c12fea4dba56df60fe5fed523
scottV-P <114790711+scottV-P@users.noreply.github.com>
2023-01-05 14:01:43 -0600

    bug fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const listNumber = document.querySelectorAll('.list-num'); //lthe class list-active
+const listNumber = document.querySelectorAll('.list-num'); 
 const inputs = document.querySelectorAll('.inputs')
 const nextBtn = document.querySelector('.next-step')
 const errorField = document.querySelectorAll('[data-error-field]')
@@ -29,7 +29,7 @@ function update(){
     }else{
         errorField[2].classList.remove('error-field-both')
         inputs[2].classList.remove('input-error')
-    listActive1()
+    listhidden1()
     hideCardOne()
    }
 
@@ -38,17 +38,19 @@ function update(){
 
 
 
-function listActive1(){
-    listNumber[0].classList.remove('lists-active')    
-    listNumber[1].classList.add('lists-active')
+function listhidden1(){
+    listNumber[0].classList.remove('lists-hidden')    
+    listNumber[1].classList.add('lists-hidden')
 }
 
 function hideCardOne(){ 
-    stepOne.classList.add('active')
-    stepTwo.classList.remove('active')
+    stepOne.classList.add('hidden')
+    stepTwo.classList.remove('hidden')
 }
 
-
+nextBtn.addEventListener('click',  ()=>{
+    update()
+});
 
 const cards = document.querySelectorAll('.plans')
 const toggler = document.querySelector('.toggler')
@@ -66,12 +68,12 @@ const monthlyContainer = document.querySelector('.monthlyContainer')
 toggler.addEventListener('click',()=>{
     const circle = document.querySelector('.circle')
     circle.classList.toggle('toggle-move')
-    monthly.classList.toggle('toggle-active')
-    yearly.classList.toggle('toggle-active')
+    monthly.classList.toggle('toggle-hidden')
+    yearly.classList.toggle('toggle-hidden')
     plansConMO.classList.toggle('plan-y-hidden')
     plansConYR.classList.toggle('plan-y-hidden')
-    yearlyContainer.classList.toggle('active')
-    monthlyContainer.classList.toggle('active')
+    yearlyContainer.classList.toggle('hidden')
+    monthlyContainer.classList.toggle('hidden')
 })
 
 
@@ -79,36 +81,38 @@ toggler.addEventListener('click',()=>{
 
 cards.forEach((item)=>{
     item.addEventListener('click',(e)=>{
-        const target = e.target
-        const lastCard = document.querySelectorAll('.selected')
-        if(lastCard){
-            lastCard[0].classList.remove('selected')
+        let target = e.target
+        let previousCard = document.querySelectorAll('.selected')
+        if(previousCard){
+            previousCard[0].classList.remove('selected')
         }
         item.classList.add('selected');
 
-        const priceMO = target.querySelector('.planPrice')
-        const priceYR = target.querySelector('.planPriceYR')
-        const title = target.querySelector('.plan-name')
+        let priceMO = target.querySelector('.planPrice')
+        let priceYR = target.querySelector('.planPriceYR')
+        let title = target.querySelector('.plan-name')
 
-        if(monthly.classList.contains('toggle-active')){
+        if(monthly.classList.contains('toggle-hidden')){
             document.querySelector('.last-time').innerHTML = "Monthly"
             document.querySelector('.total-t').innerHTML = "mo"
             document.querySelector('.service-title').innerHTML = title.innerHTML
             document.querySelector('.priceTotal').innerHTML = priceMO.innerHTML
+            document.getElementById('total-title').innerText = "Total (Monthly)"
         }
-        if(yearly.classList.contains('toggle-active')){
+        if(yearly.classList.contains('toggle-hidden')){
             document.querySelector('.last-time').innerHTML = "Yearly"
             document.querySelector('.total-t').innerHTML = "yr"
             document.querySelector('.service-title').innerHTML = title.innerHTML
             document.querySelector('.priceTotal').innerHTML = priceYR.innerHTML
+            document.getElementById('total-title').innerText = "Total (Yearly)"
         }
 
 
         nextBtnTwo.addEventListener('click',()=>{
-            stepTwo.classList.add('active')
-            stepThree.classList.remove('active')
-            listNumber[1].classList.remove('lists-active')    
-            listNumber[2].classList.add('lists-active')
+            stepTwo.classList.add('hidden')
+            stepThree.classList.remove('hidden')
+            listNumber[1].classList.remove('lists-hidden')    
+            listNumber[2].classList.add('lists-hidden')
 
             
         })
@@ -118,19 +122,11 @@ cards.forEach((item)=>{
 
 
 GoBack.addEventListener('click',()=>{
-    stepOne.classList.remove('active')
-    stepTwo.classList.add('active')
-    listNumber[1].classList.remove('lists-active')    
-    listNumber[0].classList.add('lists-active')
+    stepOne.classList.remove('hidden')
+    stepTwo.classList.add('hidden')
+    listNumber[1].classList.remove('lists-hidden')    
+    listNumber[0].classList.add('lists-hidden')
 })
-
-
-
-
-nextBtn.addEventListener('click',  ()=>{
-    update()
-});
-
 
 const featureCon = document.querySelectorAll('.features')
 const checkMark = document.querySelector('.checkboxes')
@@ -144,62 +140,63 @@ featureCon.forEach(feature=>{
         const target = event.target
         const input = target.querySelector('.checkboxes')
         input.toggleAttribute('checked')
-        feature.classList.toggle('feature-active')
+        feature.classList.toggle('feature-hidden')
 
         if(input.hasAttribute('checked')){
-        var title = target.querySelector('.header-feature').innerHTML
-        var price = target.querySelector('.price-y').innerHTML  
-        var parent = document.querySelector('.last-service-container')
-        var featureCreate = document.createElement('div')
-        featureCreate.classList.add('service')
-        parent.appendChild(featureCreate)
-        var featureContent = `<div class="service-title-s">${title}</div>
-        <div class="total-price">+$<span class="sec-ser-p">${price}</span><span class="time"></span></div>`
-        featureCreate.innerHTML = featureContent
-        feature.addEventListener('click',()=>{featureCreate.remove()})
+            var title = target.querySelector('.header-feature').innerHTML
+            var price = target.querySelector('.price-y').innerHTML  
+            var parent = document.querySelector('.last-service-container')
+            var featureCreate = document.createElement('div')
+            featureCreate.classList.add('service')
+            parent.appendChild(featureCreate)
+            var featureContent = `<div class="service-title-s">${title}</div>
+            <div class="total-price">+$<span class="sec-ser-p">${price}</span><span class="time"></span></div>`
+            featureCreate.innerHTML = featureContent
+            feature.addEventListener('click',()=>{featureCreate.remove()})
 
 
-        if(yearly.classList.contains('toggle-active')){
-            document.querySelectorAll('.time').forEach(time=>{
-                time.innerHTML = "/yr"
-            })
+            if(yearly.classList.contains('toggle-hidden')){
+                document.querySelectorAll('.time').forEach(time=>{
+                    time.innerHTML = "/yr"
+                })
 
-        }
-        if(monthly.classList.contains('toggle-active')){
-            document.querySelectorAll('.time').forEach(time=>{
-                time.innerHTML = "/mo"
-            })
-        }
-        
+            }
+            if(monthly.classList.contains('toggle-hidden')){
+                document.querySelectorAll('.time').forEach(time=>{
+                    time.innerHTML = "/mo"
+                })
+            }
+            
     }
  })
 })
     
+let totals = 0 
+
 pickBtn.addEventListener('click',()=>{
-        stepFour.classList.remove('active')
-        stepThree.classList.add('active')
-        listNumber[2].classList.remove('lists-active')    
-        listNumber[3].classList.add('lists-active')    
+        stepFour.classList.remove('hidden')
+        stepThree.classList.add('hidden')
+        listNumber[2].classList.remove('lists-hidden')    
+        listNumber[3].classList.add('lists-hidden')    
         
        
         const finalTotalitem = document.querySelector('.finalTotal-n')//total price
         const finalTotalS = document.querySelectorAll('.sec-ser-p')
         const mainPrice = document.querySelector('.priceTotal')
         
-        let totals = 0 
 
         finalTotalS.forEach(item=>{
             totals = eval (`${totals } + ${item.innerHTML}`)
             
         })
         
-        if(yearly.classList.contains('toggle-active')){
+        if(yearly.classList.contains('toggle-hidden')){
             document.querySelectorAll('.time').forEach(time=>{
                 time.innerHTML = "/yr"
             })
 
         }
-        if(monthly.classList.contains('toggle-active')){
+        if(monthly.classList.contains('toggle-hidden')){
             document.querySelectorAll('.time').forEach(time=>{
                 time.innerHTML = "/mo"
             })
@@ -215,10 +212,10 @@ pickBtn.addEventListener('click',()=>{
 
 const goBackLast = document.querySelector('#goBackButton')
 goBackLast.addEventListener('click',()=>{
-    stepTwo.classList.remove('active')
-    stepThree.classList.add('active')
-    listNumber[2].classList.remove('lists-active')    
-    listNumber[1].classList.add('lists-active')
+    stepTwo.classList.remove('hidden')
+    stepThree.classList.add('hidden')
+    listNumber[2].classList.remove('lists-hidden')    
+    listNumber[1].classList.add('lists-hidden')
 })
 
 
@@ -228,13 +225,14 @@ const goBackConfirm = document.querySelector('#goBack_confirm')
 const endSection = document.querySelector('.end-container')
 
 confirmBtn.addEventListener('click',()=>{
-    endSection.classList.remove('active')
-    stepFour.classList.add('active')
+    endSection.classList.remove('hidden')
+    stepFour.classList.add('hidden')
 })
 
 goBackConfirm.addEventListener('click',()=>{
-    stepThree.classList.remove('active')
-    stepFour.classList.add('active')
-     listNumber[3].classList.remove('lists-active')    
-    listNumber[2].classList.add('lists-active')
+    stepThree.classList.remove('hidden')
+    stepFour.classList.add('hidden')
+    listNumber[3].classList.remove('lists-hidden')    
+    listNumber[2].classList.add('lists-hidden')
+    totals = 0
 })
